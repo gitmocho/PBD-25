fe865a6a4bda3bcacec9801b859c2b60a74bc3e2
mocho <mocho@live.co.uk>
2016-04-02 06:36:36 +0100

    First commit

---
+++ PBD25.py
@@ -0,0 +1,26 @@
+
+#description     :PBD-25: Create a list containing min 15 items.
+#author          :MC
+#date            :02/04/2014
+#version         :0.1
+#usage           :python pyscript.py
+#notes           :
+#python_version  :3.4.2 
+ 
+ 
+countriesOfTheWorld = [
+'Afghanistan',
+'Aland Islands',
+'Albania',
+'Algeria',
+'American Samoa',
+'Andorra',
+'Angola',
+'Anguilla',
+'Antarctica',
+'Antigua and Barbuda',
+'Argentina',
+'Armenia',
+'Aruba',
+'Australia',
+'Austria']
